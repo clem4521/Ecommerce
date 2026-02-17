61bd7aa94a0a6f64eab552adeaef2f69210c265b
clemente <clementeahuatl@gmail.com>
2026-02-17 15:47:43 -0500

    Change home page layout

--- frontend/src/features/Products/components/ProductsGrid.jsx
+++ frontend/src/features/Products/components/ProductsGrid.jsx
@@ -2,56 +2,10 @@ import ProductContainer from "./ProductContainer.jsx";
 import "../styles/grid.css";
 function ProductsGrid(){
   return (
-    <div className="box">
+    <div className="">
       <div className="grid">
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
-        <ProductContainer name="Soccer Ball" price="30"/>
+        <ProductContainer  name="Soccer ball" price="30"/>
+        
       </div>
     </div>
   )
