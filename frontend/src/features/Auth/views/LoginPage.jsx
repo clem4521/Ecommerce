aa44183e8dade6605e0c2a6610d0fa75aecd5450
clemente <clementeahuatl@gmail.com>
2026-02-18 01:18:05 -0500

    Finish with the login and register layout

--- frontend/src/features/Auth/views/LoginPage.jsx
+++ frontend/src/features/Auth/views/LoginPage.jsx
@@ -1,20 +1,23 @@
+import "../styles/login.css";
+import "../../../shared/style/fonts.css";
+import {NavLink} from "react-router";
 function LoginPage(){
   return(
     <div className="outer-container-login">
       <div className="inner-container-login">
         <div className="">
-          <h2>Login</h2>
+          <h2 className="login-heading font-sans-serif">Login</h2>
         </div>
-        <input placeholder="Email"/>
-        <input placeholder="Password"/>
-        <div>
-          <p>Create a new account</p>
+        <input className="login-inputs" placeholder="Email"/>
+        <input className="login-inputs" placeholder="Password"/>
+        <div className="font-sans-serif login-helper">
+          <NavLink to="/auth/register" end>
+            <p className="login-create">Create a new account</p>
+          </NavLink>
+          <p className="login-create">Forgot password</p>
         </div>
-        <div>
-          <p>Forgot password</p>
-        </div>
-        <div>
-          <button>login</button>
+        <div className="login-btn-2-div">
+          <button className="login-btn-2">Login</button>
         </div>
       </div>
     </div>
