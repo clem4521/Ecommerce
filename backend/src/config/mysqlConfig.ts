33bf78ffdbd7c23808ce0cfc624e0b7b8e7b232a
clem <clementeahuatl@gmail.com>
2026-02-23 14:40:54 -0500

    fix the addProduct function

--- backend/src/config/mysqlConfig.ts
+++ backend/src/config/mysqlConfig.ts
@@ -1,14 +1,18 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+import { env } from "node:process";
 
 dotenv.config();
 
+//@ts-expect-error
 const db = await mysql.createConnection({
   host:process.env.HOST,
-  user:"root",
-  password:"4521",
+  user:process.env.USER,
+  password:process.env.PASSWORD,
   database:process.env.DB,
-  port:3306
+  port:process.env.PORT,
+  enableKeepAlive:true,
+  keepAliveInitialDelay: 10000,
 });
 
 export default db;
