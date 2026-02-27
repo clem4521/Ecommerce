81e441ae1ce9380acedec435172131feb2bc5e11
clem <clementeahuatl@gmail.com>
2026-02-26 22:46:13 -0500

    Add the product page UI

--- frontend/src/features/Products/views/ProductViewPage.jsx
+++ frontend/src/features/Products/views/ProductViewPage.jsx
@@ -1,6 +1,8 @@
 import {useEffect,useState} from "react";
 import {useParams} from "react-router";
 import axios from "axios";
+import NavBar from "../../../shared/components/NavBar/NavBar.jsx";
+
 function ProductViewPage(){
   let {productId} = useParams();
   const [price,setPrice] = useState(0);
@@ -25,9 +27,16 @@ function ProductViewPage(){
   
   return(
     <>
-      <main>
-        <h1>{name}</h1>
-        <h1>{price}</h1>
+      <NavBar/>
+      <main className="relative mt-4">
+        <img src="" alt="image" className="absolute border-2 w-[45vw] h-[60vh] ml-5"/>
+        <div className="absolute right-20 w-[20vw] cursor-default">
+          <div className="">
+            <h1 className="text-3xl">{name.charAt(0).toUpperCase()+name.slice(1)}</h1>
+            <h1 className="text-2xl font-semibold">${price}</h1>
+          </div>
+          <button className="bg-cyan-500 text-white h-[5vh] w-full rounded-2xl hover:bg-cyan-600">Buy Now</button>
+        </div>
       </main>
     </>
   )
