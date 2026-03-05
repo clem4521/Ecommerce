6e2f2430628c036d695eb8f41a75f925c57dd0a0
clem <clementeahuatl@gmail.com>
2026-03-05 16:25:47 -0500

    Added: Cart page

---
+++ frontend/src/features/Cart/Views/CartPage.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../../../shared/components/NavBar/NavBar";
+
+function CartPage(){
+    return(
+        <>
+            <NavBar/>
+            <main>
+                
+            </main>
+        </>
+    );
+}
+
+export default CartPage;
